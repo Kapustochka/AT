202ec28cf8a351de9a6a736fde08f5fea98f179c
Maxim <maximkalavrenenko@gmail.com>
2018-04-24 14:50:51 +0300

    one more method

--- lib/watir.rb
+++ lib/watir.rb
@@ -30,9 +30,20 @@ class Tests
     raise 'Wrong URI' unless @b.url == 'http://demoapp.strongqa.com/'
     raise '"Signed successfully" record wasn\'t found' unless
     (@b.div id: 'flash_notice').text == 'Signed in successfully.'
-    @b.close
+    (@b.link text: 'Logout').click
   end
+  alias login_with_correct_creds tc_02
+  def tc_03_01
+    @b.goto('http://demoapp.strongqa.com/users/sign_in')
+    test_login('admin@strongqa.com', '1234567890')
+    (@b.checkbox id: 'user_remember_me').set
+    @b.send_keys :enter
+    raise '"Signed successfully" record wasn\'t found' unless
+    (@b.div id: 'flash_notice').text == 'Signed in successfully.'
+  end
+  alias login_rememb_creds tc_03_01
 end
 
 test = Tests.new
-test.tc_02
+test.login_with_correct_creds
+test.login_rememb_creds

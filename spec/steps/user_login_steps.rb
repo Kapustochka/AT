f54226b7b021b955031c7117ac8d0de373c92f19
Maxim <maximkalavrenenko@gmail.com>
2018-05-07 11:05:44 +0300

    Instance variable are deleted, several bugs were fixed

--- spec/steps/user_login_steps.rb
+++ spec/steps/user_login_steps.rb
@@ -2,56 +2,57 @@ require 'spec_helper'
 
 module UserLoginSteps
   # Given
+  attr_accessor :login_page, :home_page
   step 'there is a :page page' do |page|
     if page =~ /login/i
-      @login_page = Login.new
+      self.login_page = Login.new
     elsif page =~ /home/i
-      @home_page = Home.new
+      self.home_page = Home.new
     end
   end
 
   step 'I am on :page page' do |page|
     if page =~ /login/i
-      @login_page.load
+      login_page.load
     elsif page =~ /home/i
-      @home_page.load
+      login_page.load
     end
   end
   # When
   step 'I click login button' do
-    if @login_page.displayed?
-      @login_page.login_button.click
-    elsif @home_page.displayed?
-      @home_page.login_button.click
+    if login_page.displayed?
+      login_page.login_button.click
+    elsif home_page.displayed?
+      home_page.login_button.click
     end
   end
 
   step 'I click logout button' do
-    @home_page.logout_button.click
+    home_page.logout_button.click
   end
 
   step 'I fill in email field with :email' do |email|
-    @login_page.email_field.set email
+    login_page.email_field.set email
   end
 
   step 'I fill in password field with :password' do |password|
-    @login_page.password_field.set password
+    login_page.password_field.set password
   end
 
   # Then
   step ':page page is displayed' do |page|
     if page =~ /login/i
-      expect(@login_page).to be_displayed
+      expect(login_page).to be_displayed
     elsif page =~ /home/i
-      expect(@home_page).to be_displayed
+      expect(home_page).to be_displayed
     end
   end
 
   step ':page page has content :content' do |page, content|
     if page =~ /login/i
-      expect(@login_page).to have_content(content)
+      expect(login_page).to have_content(content)
     elsif page =~ /home/i
-      expect(@home_page).to have_content(content)
+      expect(login_page).to have_content(content)
     end
   end
 end

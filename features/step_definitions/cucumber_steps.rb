bfa67ebb9d388316c2f8f92c9b03c1310b31dfcd
Maxim <maximkalavrenenko@gmail.com>
2018-05-08 16:09:38 +0300

    steps were redestributed

--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -1,4 +1,3 @@
-
 def login_page
   @login_page ||= Login.new
 end
@@ -11,16 +10,12 @@ Given('user is on home page') do
   home_page.load
 end
 
-When('user clicks login button on home page') do
-  home_page.login_button.click
-end
-
-Then('login page should be displayed') do
-  expect(login_page).to be_displayed
-end
-
 Given('user is on login page') do
   login_page.load
+end
+
+When('user clicks login button on home page') do
+  home_page.login_button.click
 end
 
 When('user logs in with correct creds') do
@@ -29,22 +24,10 @@ When('user logs in with correct creds') do
   login_page.login_button.click
 end
 
-Then('home page should be displayed') do
-  expect(home_page).to be_displayed
-end
-
-Then('user should see alert about successfull sign in') do
-  expect(home_page).to have_content('Signed in successfully.')
-end
-
 When('user logs in with blank password') do
   login_page.email_field.set 'admin@strongqa.com'
   login_page.password_field.set ''
   login_page.login_button.click
-end
-
-Then('user should see alert about wrong login or password') do
-  expect(login_page).to have_content('Invalid email or password.')
 end
 
 When('user logs in with blank email and password') do
@@ -75,6 +58,22 @@ When('user clicks logout button') do
   home_page.logout_button.click
 end
 
+Then('login page should be displayed') do
+  expect(login_page).to be_displayed
+end
+
 Then('user should see alert about successfull sign out') do
   expect(home_page).to have_content('Signed out successfully.')
+end
+
+Then('user should see alert about wrong login or password') do
+  expect(login_page).to have_content('Invalid email or password.')
+end
+
+Then('home page should be displayed') do
+  expect(home_page).to be_displayed
+end
+
+Then('user should see alert about successfull sign in') do
+  expect(home_page).to have_content('Signed in successfully.')
 end
